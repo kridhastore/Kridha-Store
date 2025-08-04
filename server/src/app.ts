4924c523f2cdb524a52caa123b18ccb23ca63e88
Prashant <prashanttt001@gmail.com>
2025-08-04 15:37:52 +0530

    optimized for Vercel

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db";
-import productRoutes from "./routes/product.route";
-import checkoutRoutes from "./routes/checkout.route";
-import categoryRoutes from "./routes/categories.route";
+import routes from "./routes/routes";
 
 const app = express();
 connectDB();
@@ -19,9 +17,6 @@ app.use(express.json());
 // );
 
 app.use(cors());
-
-app.use("/api/products", productRoutes);
-app.use("/api/checkout", checkoutRoutes);
-app.use("/api/categories", categoryRoutes);
+app.use("/api", routes);
 
 export default app;
